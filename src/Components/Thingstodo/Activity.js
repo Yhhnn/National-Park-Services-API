a927045ff4244bd575c2c0adbb100411fa1393b3
Yhhnn <121900959+Yhhnn@users.noreply.github.com>
2023-02-22 09:46:23 -0500

    Update Activity.js

--- src/Components/Thingstodo/Activity.js
+++ src/Components/Thingstodo/Activity.js
@@ -1,12 +1,1 @@
-import React from 'react';
 
-const Activity = ({ activity }) => {
-  return (
-    <li>
-      <h3>{activity.name}</h3>
-      <p>{activity.description}</p>
-    </li>
-  );
-};
-
-export default Activity;
